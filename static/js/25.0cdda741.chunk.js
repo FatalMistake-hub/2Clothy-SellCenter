afe31b8f904c7939b520e54889655cf512c611b9
FatalMistake-hub <79819674+FatalMistake-hub@users.noreply.github.com>
2022-11-02 16:43:17 +0700

    Updates

---
+++ static/js/25.0cdda741.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonp2Clothy-SellCenter"]=this["webpackJsonp2Clothy-SellCenter"]||[]).push([[25],{288:function(e,t,n){"use strict";n.r(t);var a=n(0),r=n.n(a),l=n(56);t.default=function(){return r.a.createElement(r.a.Fragment,null,r.a.createElement(l.a,null,"Blank"))}},56:function(e,t,n){"use strict";var a=n(0),r=n.n(a);t.a=function(e){var t=e.children;return r.a.createElement("h1",{className:"my-6 text-2xl font-semibold text-gray-700 dark:text-gray-200"},t)}}}]);
+//# sourceMappingURL=25.0cdda741.chunk.js.map
